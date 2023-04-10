2668e29a1b5a19ea579214927e4d99c18f5f4224
Alexander Krasnobokyi <87702429+AlexxRed@users.noreply.github.com>
2023-04-10 20:57:05 +0300

    Update timer.tsx

--- src/helpers/timer.tsx
+++ src/helpers/timer.tsx
@@ -1,29 +1,29 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 
 function Timer() {
     const [time, setTime] = useState({
-        minutes: 9,
-        seconds: 59,
-    });
+        minutes: 10,
+        seconds: 0,
+    })
 
     useEffect(() => {
         const timer = setInterval(() => {
-        if (time.seconds === 0) {
-            if (time.minutes === 0) {
-            clearInterval(timer);
-            alert("time is over");
+            if (time.seconds === 0) {
+                if (time.minutes === 0) {
+                    clearInterval(timer);
+                    alert('You get a 5% discount')
+                } else {
+                    setTime((prevTime) => ({
+                        minutes: prevTime.minutes - 1,
+                        seconds: 59,
+                }));
+                }
             } else {
-            setTime((prevTime) => ({
-                minutes: prevTime.minutes - 1,
-                seconds: 59,
-            }));
+                setTime((prevTime) => ({
+                ...prevTime,
+                seconds: prevTime.seconds - 1,
+                }));
             }
-        } else {
-            setTime((prevTime) => ({
-            ...prevTime,
-            seconds: prevTime.seconds - 1,
-            }));
-        }
         }, 1000);
 
         return () => clearInterval(timer);
@@ -31,9 +31,9 @@ function Timer() {
 
     return (
         <div>
-            <p>{`${time.minutes}${time.seconds < 10 ? "0" : ""} min ${time.seconds} sec to discount`}</p>
+            <p>{`${time.minutes} min ${time.seconds < 10 ? '0' : ''}${time.seconds} sec to discount`}</p>
         </div>
     );
 }
 
-export default Timer;
+export default Timer
